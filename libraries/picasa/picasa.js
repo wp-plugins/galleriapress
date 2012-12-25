2a12bffa9025661530529d14ef1a63e63beaa189
erez.odier@gmail.com <erez.odier@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-25 11:48:05 +0000

    removed no longer needes scripts from picasa.js
    git-svn-id: https://plugins.svn.wordpress.org/galleriapress/trunk@644342 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- libraries/picasa/picasa.js
+++ libraries/picasa/picasa.js
@@ -2,34 +2,6 @@ Galleriapress.picasa =
 {
     init: function()
     {
-        $('#picasa-library .browse').live('click',
-                                          function()
-                                          {
-                                              $('.picasa-loading').show();
-
-                                              var request =                                                      
-                                                  {
-                                                      action : 'picasa_library_items',
-                                                      post_id : $('#post_ID').val(),
-                                                      path : $(this).data('path')
-                                                  };
-
-                                              if($('.picasa-search').length > 0)
-                                                  request['picasa_search'] = $('.picasa-search').val();
-
-                                              $.post(ajaxurl,
-                                                     request,
-                                                     function(response)
-                                                     {
-                                                         $('#picasa-library').html(response.html);
-                                                         $('.picasa-loading').hide();
-                                                     },
-                                                     'json');
-                                          });
-    },
-
-    init_draggable: function()
-    {
         $('#picasa-library .grid > li').draggable(
 						{
 								appendTo: 'body',
@@ -45,6 +17,11 @@ Galleriapress.picasa =
                     Galleriapress.hide_drag_message();
                 }
 						});
+    },
+
+    after_load_library_path: function()
+    {
+        this.init();
     }
 
 };
